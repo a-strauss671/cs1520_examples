4e15bc18f7dc7c496cc4206d529f33e4afd7a874
Brian Nixon <btn10@pitt.edu>
2026-09-15 16:22:06 -0400

    Add stubbed jsx files

---
+++ react/re01_state/src/IndependentButton.jsx
@@ -0,0 +1,12 @@
+// TODO: Create the IndependentButton component in this file
+// such that each instance of the component maintains its own
+// state (a count) and increments the count when the button
+// is clicked. 
+
+export default function IndependentButton() {
+	// Replace the div element below with the necessary JSX
+	return (
+		<div>
+		</div>
+	);
+}
---
+++ react/re01_state/src/SharedButtons.jsx
@@ -0,0 +1,11 @@
+// TODO: Add the SharedButtons component to this file such that
+// it initializes two SharedButton components that share a count, which is
+// passed via props.
+
+export default function SharedButtons() {
+	// Replace the div below with the necessary JSX
+	return (
+		<div>
+		</div>
+	);
+}
